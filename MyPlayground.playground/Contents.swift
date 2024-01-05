260b3064ebee75164c441c3cc28c6fd823ea3905
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-05 09:11:08 +0700

    Memulai pemrograman dengan swift dengan dicoding

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+print(greeting)
+
+//variable and constant
+let maximumLogginAttemps :Int = 10 //tidak bisa berubah
+var loginAttemps :Int = 10 //bisa berubah
+print(loginAttemps)
+
+//deklarasi multi
+var nama :String = "bayu", alamat :String = "danareja";
+var dataDiri = nama + alamat
+print(dataDiri)
+print(nama+alamat)
