85336b2dbcd77ff8be4bace0648a24265f66f711
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-05 09:15:20 +0700

    float vs double

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,8 @@ var nama :String = "bayu", alamat :String = "danareja";
 var dataDiri = nama + alamat
 print(dataDiri)
 print(nama+alamat)
+
+//floating number vs double
+let apple = 2.4
+let banana: Double = 3.0 //64bit
+let melon:Float = 5.4 //32bit
