e4ac84246f81681c6bb3882c46a185c367ee1017
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-09 10:11:44 +0700

    clousure capturing value

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -258,3 +258,22 @@ print(reversedNames)
 var reversedNames2 = names.sorted(by: >)
 print(reversedNames2)
 
+//Clousure Capturing value
+func makeIncrement(forIncrement amount: Int)-> ()-> Int {
+    var runningTotal = 0
+    func incrementer()-> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrement(forIncrement: 10)
+print(incrementByTen())
+print(incrementByTen())
+print(incrementByTen())
+let incrementBySeven = makeIncrement(forIncrement: 7)
+print(incrementBySeven())
+
+print(incrementByTen())
+print(incrementBySeven())
