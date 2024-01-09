eb8d74b5e37ee5602cdd1722a3b1af5af358ac1e
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-09 18:13:31 +0700

    Object Oriented Programming class vs struct

--- ObjectOrientedProgramming.playground/Contents.swift
+++ ObjectOrientedProgramming.playground/Contents.swift
@@ -3,7 +3,7 @@ import Cocoa
 //MARK: Define resolution vs class
 struct Resolution{
     var width = 0
-    var heigth = 0
+    var height = 0
 }
 class videoMode{
     var resolution = Resolution()
@@ -20,3 +20,31 @@ print("Width dari struct \(someResulution.width)")
 print("Width dari struct \(someVideoMode.resolution.width)")
 
 someVideoMode.resolution.width = 1280
+
+//MARK: Struct merupakan type values artinya
+let hd = Resolution(width: 640, height: 480)
+var cinema = hd //maka ini akan mencopy hd tanpa mereference nantinya
+
+cinema.width = 120
+
+print("width vga : \(hd.width)")
+print("width cinema : \(cinema.width)")
+
+//MARK: Sedangkan class merupakan tipe data reference
+let tenEighty = videoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty //not copy but also reference class videomode
+alsoTenEighty.frameRate = 30.0
+
+if tenEighty === alsoTenEighty {
+   print("tenEighty and alsoTenEighty refer to the same VideoMode instance.")
+}
+
+print("tenEight: \(tenEighty.frameRate)")
+print("alsoTenEight: \(alsoTenEighty.frameRate)")
+
+
