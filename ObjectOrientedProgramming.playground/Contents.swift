8629f793be23e994161c6269583691a1bc537a0a
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-10 10:13:23 +0700

    stored properti

--- ObjectOrientedProgramming.playground/Contents.swift
+++ ObjectOrientedProgramming.playground/Contents.swift
@@ -41,10 +41,47 @@ let alsoTenEighty = tenEighty //not copy but also reference class videomode
 alsoTenEighty.frameRate = 30.0
 
 if tenEighty === alsoTenEighty {
-   print("tenEighty and alsoTenEighty refer to the same VideoMode instance.")
+    print("tenEighty and alsoTenEighty refer to the same VideoMode instance.")
 }
 
 print("tenEight: \(tenEighty.frameRate)")
 print("alsoTenEight: \(alsoTenEighty.frameRate)")
+
+
+// MARK: Properti (stored & computed)
+// Stored Properti
+struct FixedLengthRange{
+    var firstValue: Int //Variable stored properti
+    let length: Int //Constanta stored properti
+}
+
+//Stored variable structure instance
+var rangeOfThereeItems = FixedLengthRange(firstValue: 0, length: 40)
+print("jangkauan tersebut memiliki nilai : \(rangeOfThereeItems)")
+
+rangeOfThereeItems.firstValue = 5
+print("sekarang jangkauannya adalah : \(rangeOfThereeItems)")
+
+//Stored constant structure instance
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+//rangeOfFourItems.firstValue = 2 (ini error walaupun firstValue var, karena instance let
+// hal ini karena struct merupakan value type, berbeda dengan kelas
+
+//lazy stored properti
+class DataImporter{
+    var filename = "data.txt"
+}
+class DataManager{
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+let manager = DataManager()
+manager.data.append("some data")
+manager.data.append("some more data")
+
+print(manager.importer.filename)
+// instance DataImporter untuk properti importer kini telah dibuat
+// Menyetak "data.txt"
+
 
 
