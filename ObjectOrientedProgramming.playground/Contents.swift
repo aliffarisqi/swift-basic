2ef4271a14b6e61918014585136b2dff8d655247
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-10 10:13:55 +0700

    computed properti

--- ObjectOrientedProgramming.playground/Contents.swift
+++ ObjectOrientedProgramming.playground/Contents.swift
@@ -84,4 +84,20 @@ print(manager.importer.filename)
 // Menyetak "data.txt"
 
 
+//MARK: Computed Properti
+struct Point{
+    var x = 0.0, y = 0.0
+}
 
+struct shape{
+    var origin = Point()
+    var center: Point{
+        get{
+            return Point(x: origin.x/2, y: origin.y/2)
+        }
+        set(newCenter){
+            origin.x = newCenter.x/2
+            origin.y = newCenter.y/2
+        }
+    }
+}
