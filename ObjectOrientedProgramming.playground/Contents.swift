7823a24ef2146ddcd2aeb87dfbbde544b3b968d1
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-10 10:14:23 +0700

    Properti Observer

--- ObjectOrientedProgramming.playground/Contents.swift
+++ ObjectOrientedProgramming.playground/Contents.swift
@@ -101,3 +101,23 @@ struct shape{
         }
     }
 }
+
+//MARK: Properti Observer
+class StepCounter{
+    var totalSteps: Int = 0{
+        willSet(newSteps){
+            print("About to set totalSteps to \(newSteps)")
+        }
+        didSet{
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 50
+
+stepCounter.totalSteps = 150
+
+stepCounter.totalSteps = 420
